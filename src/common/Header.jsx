02b3b41c309dc12660b4245997f10ad3f7c9b2ce
liuzuolin1996 <liuzuolin1996@gmail.com>
2021-12-22 00:38:15 +0800

    added landing page

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 function Header() {
     
     return (
-        <nav class="navbar navbar-light bg-light">
+        <nav class="navbar">
             <div class="container-fluid">
 
                 <div>Test1</div>
@@ -13,7 +13,6 @@ function Header() {
                     <li class="nav-item">
                         <Link to="/" class="nav-link">Home</Link>
                     </li>
-
 
                     <li class="nav-item">
                         <Link to="/about" class="nav-link">About</Link>
