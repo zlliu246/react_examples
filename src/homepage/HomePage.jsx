3f3e10394be9561d45f4772f6c8e1a16573b00df
liuzuolin1996 <liuzuolin1996@gmail.com>
2022-01-04 17:35:40 +0800

    added mobile-responsiveness stuff

--- src/homepage/HomePage.jsx
+++ src/homepage/HomePage.jsx
@@ -1,7 +1,51 @@
-function HomePage() {
-    return <div>
-        This is the home page
-    </div>
+import React from "react"
+
+class HomePage extends React.Component {
+
+    state = {
+        innerWidth: window.innerWidth
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this))
+        this.resize()
+    }
+
+    resize() {
+        this.setState({innerWidth: window.innerWidth})
+    }
+
+    getColor(innerWidth) {
+        if (innerWidth <= 500) return "red"
+        if (innerWidth <= 600) return "blue"
+        if (innerWidth <= 700) return "green"
+        if (innerWidth <= 800) return "yellow"
+        if (innerWidth <= 900) return "pink"
+        if (innerWidth <= 1000) return "beige"
+        if (innerWidth <= 1100) return "lightblue"
+        return "violet"
+    }
+
+    render() {
+
+        const style = {
+            "background-color": this.getColor(this.state.innerWidth)
+        }
+
+        return <div>
+            <h1>This is the home page</h1>
+            <h1>
+                this.state.innerWidth: {this.state.innerWidth}
+            </h1>
+
+
+            <h1 style={style}>
+                The background color changes as you resize
+            </h1>
+
+        </div>
+    }
+
 }
 
 export default HomePage
